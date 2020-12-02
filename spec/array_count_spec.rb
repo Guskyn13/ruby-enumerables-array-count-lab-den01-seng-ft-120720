75c7857ae51578c6102cf68482cbeaf940db8544
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-02 09:45:16 +0000

    Automatically backed up by Learn

--- spec/array_count_spec.rb
+++ spec/array_count_spec.rb
@@ -7,11 +7,10 @@ describe 'count_strings' do
   it 'takes in an array of varying data types and returns the total number of strings' do
     expect(count_strings(array_1)).to eq(3)
   end
-end
+
 
 
 describe 'count_empty_strings' do
   it 'takes in an array of varying data types and returns the total number of EMPTY strings' do
     expect(count_empty_strings(array_1)).to eq(1)
   end
-end
