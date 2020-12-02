17c7fcdae78a9fde44bcf78e0dfc8188857c9413
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-02 09:43:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,5 @@
-
-
-def count_empty_strings(array)
-  array = ["", "hello", "world", 4, 1, 5, [], {}]
-  array.count do |element|
-    element == ""
+def count_strings(array)
+array = ["", "hello", "world", 4, 1, 5, [], {}]
+array.count do |element|
+  element != ""
 end
