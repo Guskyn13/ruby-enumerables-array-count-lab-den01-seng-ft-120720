84fb677a6e7d6a1750e74c91f8563966b543b31c
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-02 09:21:28 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,12 +3,3 @@ array = ["", "hello", "world", 4, 1, 5, [], {}]
 array.count do |element|
   element != ""
 end
-
-def count_empty_strings(array)
-array = ["", 4, "", "goodbye",""]
-arrray.count {|empty_strings| empty_strings = ""}
-end
-
-
-
-!= (not equal)
